1e5f4b95c2465caa0d3495a19370797758f551c3
name <moesanaye.ptuec@gmail.com>
2021-09-24 21:13:11 +0630

    myvuepj-1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>hi</h1>
+<!--step3-->
+<!--similar to write <Modal/>-->
+<!--props parent to child-->
+<div v-if="showModal">
+  <Modal :header="header" :content="content" :theme="theme" @close="showModal=!showModal">
+    <h1>Login success</h1>
+    <p>welcome user moe san aye</p>
+  </Modal>
+</div>
+<!--event modifier behind event(.) nae write-->
+<button @click="showModal=!showModal">open modal</button><br>
+
+<div v-if="deleteModal">
+  <Modal :header="header" :content="content" :theme1="theme1" @close="deleteModal=!deleteModal">
+    <h1>Are you sure want to delete?</h1>
+    <p>User MoeSanAye</p>
+    <p>Thanks, User.</p>
+  </Modal>
+</div>
+<!--event modifier behind event(.) nae write-->
+<button @click="deleteModal=!deleteModal">delete modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//step1 do import
+import Modal from './components/Modal.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      header:"Login success",
+      content:"welcome user Moe San Aye",
+      theme:"success",
+      theme1:"delete",
+      showModal:false,
+      deleteModal:false
+    }
+  },
+  //step2 register
+  components:{
+    //similar write(Modal)
+    Modal:Modal
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+    /* h1{
+        color: #3e3f;
+      } */
+      button{ 
+        margin: 5px;
+      }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+      <div class="modal" :class="{success:theme==='success', danger:theme1==='delete'}">
+        <!--<h1>{{header}}</h1>
+          <p>{{content}}</p>-->
+          <slot/>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+    //receive props data from parent
+    props:['header', 'content', 'theme', 'theme1'],
+    methods:{
+        //event emit child to parent prepare data
+        closeModal(){
+           this.$emit("close")
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .backdrop{
+        position: fixed;
+        top: 0;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background-color: #fff;
+        border-radius: 10px;
+    }
+    p{
+        font-style: normal;
+    }
+    .success{
+        background-color: rgb(50, 207, 10);
+        color:#ffff;
+    }
+    .danger{
+        background-color: crimson;
+        color: #fff;
+    }
+</style>
